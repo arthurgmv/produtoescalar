789855f35212254ae849b57d013d95a33829f2e3
Arthur Gabriel <123274385+arthurgmv@users.noreply.github.com>
2024-06-03 08:30:18 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+      function calcularProdutoEscalar() {
+        var vetor1 = [];
+        var vetor2 = [];
+        for (var i = 1; i <= 10; i++) {
+          vetor1.push(parseInt(document.getElementById("vetor1-" + i).value));
+          vetor2.push(parseInt(document.getElementById("vetor2-" + i).value));
+        }
+        var produtoEscalar = 0;
+        for (var j = 0; j < 10; j++) {
+          produtoEscalar += vetor1[j] * vetor2[j];
+        }
+        document.getElementById("resultado").innerHTML = "O produto escalar entre os vetores é: " + produtoEscalar;
+      }
+ 
